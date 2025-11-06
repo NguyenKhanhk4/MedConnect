d27bdaed3cbae52284e893bfc1b8ae11ffe9668f
VuViet06 <vudinhviet162@gmail.com>
2025-11-06 10:12:36 +0700

    update code

--- medconnect/server/models/payment.model.js
+++ medconnect/server/models/payment.model.js
@@ -132,7 +132,6 @@ const PaymentSchema = new Schema(
 
 // Index để query nhanh
 // Compound index cho phép nhiều payment cho 1 appointment (booking + service)
-// NOT unique - allows multiple payments per appointment
 PaymentSchema.index({ appointmentId: 1, invoiceType: 1 }, { unique: false });
 PaymentSchema.index({ pendingOrderCode: 1 });
 
